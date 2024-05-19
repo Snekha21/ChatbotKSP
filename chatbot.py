33100a77d170b595dafa1b69d463853f0f842322
Snekha Suresh <snekhasuresh@Snekhas-Mac.local>
2024-05-19 15:34:44 +0530

    theme added

--- chatbot.py
+++ chatbot.py
@@ -8,6 +8,15 @@ genai.configure(api_key="AIzaSyA3pzFiHktKXSwOhnSsLzEE6dNJb-QptI8")
 ## function to load Gemini Pro model and get repsonses
 model=genai.GenerativeModel("gemini-pro") 
 chat = model.start_chat(history=[])
+st.set_page_config(layout="wide",page_title="VISION PRO")
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+st.header('Chatbot')
 def get_gemini_response(question):
     
     response=chat.send_message(question,stream=True)
